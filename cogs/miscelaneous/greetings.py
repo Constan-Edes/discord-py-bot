e014fb3064b1074ae0f1dac38c5edba84ec0423e
constantinoedes <constantinoedes@gmail.com>
2024-05-31 17:20:02 +0000

    Update greeting command

--- cogs/miscelaneous/greetings.py
+++ cogs/miscelaneous/greetings.py
@@ -1,7 +1,6 @@
 from discord.ext.commands import Bot
 from discord import Interaction, app_commands
 from core.cog_base import CogBase
-from api.clients import openai_client
 
 class Greetings(CogBase, name="Greetings module", description="Bot presentations for testing purposes"):
     def __init__(self, bot: Bot):
@@ -14,17 +13,7 @@ class Greetings(CogBase, name="Greetings module", description="Bot presentations
 
     @app_commands.command(name="hello", description="Sends greeting")
     async def hello(self, interaction: Interaction) -> None:
-        completion = openai_client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": "Say \"Hello!\" in a random language",
-                }
-            ],
-            max_tokens = 10,
-            model="gpt-3.5-turbo",
-        )
-        await interaction.response.send_message(completion.choices[0].message.content)
+        await interaction.response.send_message("Hi! 🙋")
 
 async def setup(bot: Bot) -> None:
     await Greetings(bot).setup()
